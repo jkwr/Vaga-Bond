7347516d4ea2ca96abca00deefab3a8362eff215
MaluPalu <malu.peralta808@gmail.com>
2017-09-21 11:26:51 -0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# t.string “title”
+# t.string “description”
+# t.bigint “city_id”
+# t.datetime “created_at”, null: false
+# t.datetime “updated_at”, null: false
+# t.index [“city_id”], name: “index_posts_on_city_id”

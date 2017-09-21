abf64401fb1c753dfe80c550779dc8839c18ae0a
Jevell Rollins <jkwr92@gmail.com>
2017-09-21 09:07:38 -0700

    login CSS done

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show', as: 'user'
 
   get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
   post '/sessions', to: 'sessions#create'
 end

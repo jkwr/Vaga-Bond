61f17554cf9702ab3e47954f49998eb61f427a1f
Jevell Rollins <jkwr92@gmail.com>
2017-09-22 17:30:13 -0700

    button drop down is working

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,5 +1,17 @@
 module CitiesHelper
+
+  # returns the City object based on dashed name
   def get_city_from_name(name)
     City.where('lower(location) = ?', name.downcase.gsub('-',' ')).first
+  end
+
+  # returns the dashed name from a City object
+  def get_name_from_city(city)
+    city.location.downcase.gsub(' ','-')
+  end
+
+  # returns the dashed show path for a City object
+  def get_path_from_city(city)
+  	"/cities/#{get_name_from_city(city)}"
   end
 end

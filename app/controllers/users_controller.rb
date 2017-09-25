0d2d6b02e34a723ea22aacc0d18c4368184b8a95
Jevell Rollins <jkwr92@gmail.com>
2017-09-25 14:57:00 -0700

    User show page bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find_by_id(params[:id])
-		@posts = Post.find_by_id(params[:id])
+		# @posts = Post.find_by_id(params[:id])
+		@posts = @user.posts 
 	end
 
 	def update
